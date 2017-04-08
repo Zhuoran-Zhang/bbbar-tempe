9b355f8d7a81f62ce77b26eeffbeb4e998c1ba33
jjlovekdw <jjlovekdw@gmail.com>
2017-04-08 21:55:48 +0200

    SQL
    BDD

--- projet-bbbar-temp/sql/bbbar_tempe.sql
+++ projet-bbbar-temp/sql/bbbar_tempe.sql
@@ -1,26 +1,43 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.4.10
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 21, 2017 at 08:38 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
-
-CREATE DATABASE bbbar_tempe;
+-- Host: localhost:8889
+-- Generation Time: Apr 08, 2017 at 09:32 PM
+-- Server version: 5.5.42
+-- PHP Version: 5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `bbbar_tempe`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_games`
+--
+
+CREATE TABLE `tbl_games` (
+  `roomId` int(5) NOT NULL,
+  `playerName` varchar(20) NOT NULL,
+  `score` int(10) NOT NULL,
+  `allPlayersOK` tinyint(1) NOT NULL,
+  `caractereOK` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_games`
+--
+
+INSERT INTO `tbl_games` (`roomId`, `playerName`, `score`, `allPlayersOK`, `caractereOK`) VALUES
+(2, 'dsss', 200, 1, 0),
+(2, 'dqsqsqsd', 0, 1, 0),
+(1, 'bhbiu', 600, 0, 0),
+(1, 'knkjnlkn', 100, 0, 0),
+(1, 'vjhvh', 100, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -33,7 +50,7 @@ CREATE TABLE `tbl_user` (
   `username` varchar(50) NOT NULL,
   `pword` varchar(6) NOT NULL,
   `role` varchar(20) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_user`
@@ -43,7 +60,8 @@ INSERT INTO `tbl_user` (`id`, `username`, `pword`, `role`) VALUES
 (1, 'boss', 'boss', 'boss'),
 (2, 'alan', 'alan', 'boss'),
 (3, 'iris', 'iris', 'guest'),
-(4, 'host', 'host', 'host');
+(4, 'host', 'host', 'host'),
+(5, 'guest', 'guest', 'guest');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +81,4 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
